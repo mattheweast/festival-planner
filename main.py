efa7880adad0b0a2fc774fdd228f6536a713641e
mattheweast <account.matteast@icloud.com>
2025-06-10 13:44:14 +0100

    Updated requirements

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
 
+# FastAPI Instance
 app = FastAPI()
 
 # Route / Path Operation
 @app.get("/")
 def root():
     return{"message": "Welcome to my API"}
+
+@app.get("/posts")
+def get_posts():
+    return{"data": "You're Posts"}
