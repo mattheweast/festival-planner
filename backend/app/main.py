ad07bb7babadcc2ff993703f1c7d180b07873da3
mattheweast <account.matteast@icloud.com>
2025-07-17 12:39:51 +0100

    Added DB connection

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,25 @@
 from fastapi import FastAPI
 from backend.app.routers.post_router import post_router
-
+import psycopg2
+from psycopg2.extras import RealDictCursor
 
 app = FastAPI()
 
 app.include_router(post_router)
+
+try:
+    conn = psycopg2.connect(
+        host='localhost',
+        database='fastapi',
+        user='matteast',
+        password='admin',
+        cursor_factory=RealDictCursor
+        )
+    cursor = conn.cursor()
+    print("Database connection was succesfull!")
+except Exception as error:
+    print("Connecting to database failed!")
+    print("Error: ", error)
 
 
 @app.get("/")
--- backend/app/schemas/post_schema.py
+++ backend/app/schemas/post_schema.py
@@ -1,9 +1,7 @@
 from pydantic import BaseModel
-from typing import Optional
 
 
 class Post(BaseModel):
     title: str
     content: str
     published: bool = True
-    rating: Optional[int] = None
