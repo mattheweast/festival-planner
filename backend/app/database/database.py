e8060e996dfb37897c063bba8a71b1f9e6a714b7
mattheweast <account.matteast@icloud.com>
2025-07-23 16:56:54 +0100

    Removed Test, will add later

--- backend/app/database/database.py
+++ backend/app/database/database.py
@@ -1,12 +1,15 @@
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
+import os
+
+DB_NAME = os.getenv("DB_NAME", "fastapi")
 
 while True:
     try:
         conn = psycopg2.connect(
             host='localhost',
-            database='fastapi',
+            database=DB_NAME,
             user='matteast',
             password='admin',
             cursor_factory=RealDictCursor
